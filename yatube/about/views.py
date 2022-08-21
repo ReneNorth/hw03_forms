c426321045d2b70cd6b9dcceb9422c343e0d49db
Yury Zharikov <zharikovyuri@gmail.com>
2022-08-21 16:54:27 +0300

    the test are passed

---
+++ yatube/about/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+
+class AboutAuthorView(TemplateView):
+    template_name = 'about/author.html'
+#    return render(request, 'about/author.html', name='author')
+
+
+class AboutTechView(TemplateView):
+    template_name = 'about/tech.html'
+#    return render(request, 'about/tech.html', name='tech')
