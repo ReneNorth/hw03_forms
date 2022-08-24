1eb09f5a65da4e9aee9cb3a5aaee44ecaee9e130
Yury Zharikov <zharikovyuri@gmail.com>
2022-08-24 11:19:17 +0300

    after review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Group
 from .forms import PostForm
-# from django.views.generic.edit import CreateView
 from yatube.settings import DEF_NUM_POSTS
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -12,11 +11,16 @@ User = get_user_model()
 group_list = Group.objects.all().values_list('title', flat=True).distinct()
 
 
-def index(request):
-    post_list = Post.objects.all().order_by('-pub_date')
-    paginator = Paginator(post_list, DEF_NUM_POSTS)
+def paginator(list_of_posts, request, posts_per_page):
+    paginator = Paginator(list_of_posts, posts_per_page)
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    pages = paginator.get_page(page_number)
+    return pages
+
+
+def index(request):
+    post_list = Post.objects.all()
+    page_obj = paginator(post_list, request, DEF_NUM_POSTS)
     context = {
         'page_obj': page_obj,
         'group_list': group_list
@@ -26,10 +30,8 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    post_list = Post.objects.filter(group=group).all().order_by('-pub_date')
-    paginator = Paginator(post_list, DEF_NUM_POSTS)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    post_list = group.posts.all()
+    page_obj = paginator(post_list, request, DEF_NUM_POSTS)
     context = {
         'page_obj': page_obj,
         'group': group,
@@ -39,10 +41,8 @@ def group_posts(request, slug):
 
 def profile(request, username):
     post_list = Post.objects.filter(
-        author__username=username).order_by('-pub_date')
-    paginator = Paginator(post_list, DEF_NUM_POSTS)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+        author__username=username)
+    page_obj = paginator(post_list, request, DEF_NUM_POSTS)
     author = get_object_or_404(User, username=username)
     num_of_posts = Post.objects.filter(author__username=username).count()
 
@@ -56,25 +56,22 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    num_of_posts = Post.objects.filter(author=post.author).count()
     context = {
         'post': post,
-        'num_of_posts': num_of_posts,
     }
     return render(request, 'posts/post_detail.html', context)
 
 
 @login_required
 def post_create(request):
+    form = PostForm(request.POST or None)
     if request.method == 'POST':
-        form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             form.save()
             return redirect('posts:profile', post.author)
         return render(request, 'posts/post_create.html', {'form': form})
-    form = PostForm()
     return render(request, 'posts/post_create.html',
                   {'form': form, 'group_list': group_list})
 
@@ -85,15 +82,11 @@ def post_edit(request, post_id):
     is_edit = True
     if post.author != request.user:
         return redirect('posts:post_detail', post_id)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('posts:post_detail', post_id)
-        return render(request, 'posts/post_create.html', {'form': form})
-
-    form = PostForm(instance=post)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
+        return redirect('posts:post_detail', post_id)
     context = {
         'form': form,
         'post_id': post_id,
