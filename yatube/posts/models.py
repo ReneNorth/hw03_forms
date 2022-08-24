face9a18bc7445de5b669a4f91a872efa5ecd55d
Yury Zharikov <zharikovyuri@gmail.com>
2022-08-24 11:28:49 +0300

    after review3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -31,7 +31,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts',   
+        related_name='posts',
         verbose_name='Автор',
     )
 
